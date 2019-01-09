3a937ca58dddab5fbf4a851a14dadb47709e73b9
bereket6725 <bereket6725@gmail.com>
2019-01-08 20:22:02 -0500

    added files for models, views, controllers, ViewModels and Coordinators and added a tabBarController with five tabs to the main.storyboard

---
+++ WorldScoop/WorldScoop/ViewControllers/EU+AustraliaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EU+AustraliaViewController.swift
+//  WorldScoop
+//
+//  Created by Bereket Ghebremedhin  on 1/8/19.
+//  Copyright © 2019 Bereket Ghebremedhin . All rights reserved.
+//
+
+import UIKit
+
+class EU_AustraliaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WorldScoop/WorldScoop/ViewControllers/SouthAmericaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SouthAmericaViewController.swift
+//  WorldScoop
+//
+//  Created by Bereket Ghebremedhin  on 1/8/19.
+//  Copyright © 2019 Bereket Ghebremedhin . All rights reserved.
+//
+
+import UIKit
+
+class SouthAmericaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
