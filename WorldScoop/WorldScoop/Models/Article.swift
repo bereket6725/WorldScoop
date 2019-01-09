6271ed8720a6ed5df0d5f8c64315eeac7f190b6e
bereket6725 <bereket6725@gmail.com>
2019-01-08 21:20:24 -0500

    added article model, articleBox for decoding and coding keys

--- WorldScoop/WorldScoop/Models/Article.swift
+++ WorldScoop/WorldScoop/Models/Article.swift
@@ -8,3 +8,43 @@
 
 import Foundation
 
+struct Article: Codable {
+    let source: String
+    let author: String
+    let title: String
+    let description: String
+    let imageURLString: URL
+    let URLString: URL
+    let publishedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case source = "name"
+        case author
+        case title
+        case description
+        case imageURLString = "urlToImage"
+        case URLString = "url"
+        case publishedAt
+    }
+}
+
+enum Continent {
+    case NorthAmerica
+    case SouthAmerica
+    case EuropeAndAustralia
+    case Africa
+    case Asia
+}
+
+extension Article {
+    //
+    
+}
+
+public struct ArticleBox: Codable {
+    let articles: [Article]
+    
+    enum CodingKeys: String, CodingKey {
+        case articles = "data"
+    }
+}
